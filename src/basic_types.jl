19f631b85fbbef140e62990e9e23177ac7411184
Kristian Holme <holme.kristian@gmail.com>
2025-05-05 20:36:35 +0200

    first commit

---
+++ src/basic_types.jl
@@ -0,0 +1,15 @@
+abstract type AbstractEnv end
+
+
+abstract type AbstractSpace end
+
+
+abstract type AbstractAgent end
+
+
+
+function learn(agent::AbstractAgent, env::AbstractEnv)
+end
+
+
+
---
+++ src/spaces.jl
@@ -0,0 +1,1 @@
+abstract type AbstractBox <: AbstractSpace end
