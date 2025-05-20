f175303d78d4cb8b9f3bff8eacd0aa45430f3dde
Kristian Holme <holme.kristian@gmail.com>
2025-05-20 10:46:42 +0200

    add test for initial  ratio

--- test/test_algorithms.jl
+++ test/test_algorithms.jl
@@ -1,6 +1,22 @@
 using Test
 using DRiL
+using DRiL.Pendulum
 
 @testset "algorithms.jl" begin
-    # TODO: Add tests for algorithms
-end 
+    # Test: logprobs are consistent after rollout
+    env = PendulumEnv() |> ScalingWrapperEnv
+    policy = ActorCriticPolicy(observation_space(env), action_space(env))
+    agent = ActorCriticAgent(policy; n_steps=8, batch_size=8, epochs=1, verbose=0)
+    alg = PPO()
+    n_steps = agent.n_steps
+    n_envs = 1
+    roll_buffer = RolloutBuffer(observation_space(env), action_space(env), alg.gae_lambda, alg.gamma, n_steps, n_envs)
+    DRiL.collect_rollouts!(roll_buffer, agent, env)
+    obs = roll_buffer.observations
+    act = roll_buffer.actions
+    logprobs = roll_buffer.logprobs
+    ps = agent.train_state.parameters
+    st = agent.train_state.states
+    _, new_logprobs, _, _ = evaluate_actions(policy, obs, act, ps, st)
+    @test isapprox(vec(logprobs), vec(new_logprobs); atol=1e-5, rtol=1e-5)
+end
