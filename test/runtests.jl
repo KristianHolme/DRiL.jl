0aacb94f82126712f13d930501e2d6b04985fe0a
Kristian Holme <holme.kristian@gmail.com>
2025-05-20 11:32:28 +0200

    update tests to use testitems, move log prob testing from alg to buffers

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,36 @@ using Test
 using Aqua
 using JET
 
-@testset "DRiL.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(DRiL)
-    end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(DRiL; target_defined_modules = true)
-    end
-    # Write your tests here.
+@testitem "Code quality (Aqua.jl)" begin
+    Aqua.test_all(DRiL)
 end
 
-# using Test
-# include("test_buffers.jl")
-# include("test_policies.jl")
-# include("test_agents.jl")
-# include("test_algorithms.jl")
-# include("test_environments.jl")
-# include("test_utils.jl")
-# include("test_spaces.jl")
-# include("test_basic_types.jl")
+@testitem "Code linting (JET.jl)" begin
+    JET.test_package(DRiL; target_defined_modules=true)
+end
+
+@testitem "buffers.jl" begin
+    include("test_buffers.jl")
+end
+
+# @testitem "policies.jl" begin
+#     include("test_policies.jl")
+# end
+# @testitem "agents.jl" begin
+#     include("test_agents.jl")
+# end
+# @testitem "algorithms.jl" begin
+#     include("test_algorithms.jl")
+# end
+# @testitem "environments.jl" begin
+#     include("test_environments.jl")
+# end
+# @testitem "utils.jl" begin
+#     include("test_utils.jl")
+# end
+# @testitem "spaces.jl" begin
+#     include("test_spaces.jl")
+# end
+# @testitem "basic_types.jl" begin
+#     include("test_basic_types.jl")
+# end
--- test/test_buffers.jl
+++ test/test_buffers.jl
@@ -1,6 +1,24 @@
 using Test
 using DRiL
-
+using Pendulum
 @testset "buffers.jl" begin
-    # TODO: Add tests for buffers
-end 
+    # Test: logprobs are consistent after rollout
+    pend_env() = PendulumEnv() |> ScalingWrapperEnv
+    env = MultiThreadedParallelEnv([pend_env() for _ in 1:4])
+    policy = ActorCriticPolicy(observation_space(env), action_space(env))
+    agent = ActorCriticAgent(policy; n_steps=8, batch_size=8, epochs=1, verbose=0)
+    alg = PPO()
+    n_steps = agent.n_steps
+    n_envs = 1
+    roll_buffer = RolloutBuffer(observation_space(env), action_space(env), alg.gae_lambda, alg.gamma, n_steps, n_envs)
+    for i in 1:10
+        DRiL.collect_rollouts!(roll_buffer, agent, env)
+        obs = roll_buffer.observations
+        act = roll_buffer.actions
+        logprobs = roll_buffer.logprobs
+        ps = agent.train_state.parameters
+        st = agent.train_state.states
+        _, new_logprobs, _, _ = evaluate_actions(policy, obs, act, ps, st)
+        @test isapprox(vec(logprobs), vec(new_logprobs); atol=1e-5, rtol=1e-5)
+    end
+end
