5e8c91bbca40be4ec9102ab8151fd66aac042374
Kristian Holme <holme.kristian@gmail.com>
2025-06-24 17:56:33 +0200

    adjust callbacks

--- src/callbacks.jl
+++ src/callbacks.jl
@@ -1,5 +1,15 @@
-on_training_start(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, alg::AbstractAlgorithm, iterations::Int, total_steps::Int) = true
-on_rollout_start(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, rollout_buffer::AbstractBuffer) = true
-on_step(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, trajectories::Vector{DRiL.Trajectory}, observations::Vector{Any}) = true
-on_rollout_end(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, rollout_buffer::AbstractBuffer) = true
-on_training_end(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, alg::AbstractAlgorithm) = true
+function on_training_start(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, alg::AbstractAlgorithm, iterations::Int, total_steps::Int)
+    return true
+end
+function on_rollout_start(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, rollout_buffer::AbstractBuffer)
+    return true
+end
+function on_step(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, trajectories::Vector{<:Trajectory}, observations::Vector)
+    return true
+end
+function on_rollout_end(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, rollout_buffer::AbstractBuffer)
+    return true
+end
+function on_training_end(callback::AbstractCallback, agent::AbstractAgent, env::AbstractParallelEnv, alg::AbstractAlgorithm)
+    return true
+end
