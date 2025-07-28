bcff98ddb5ab83dc2e6a642464a08476c77a6773
Kristian Holme <holme.kristian@gmail.com>
2025-07-29 00:17:39 +0200

    Sac now runs, but with lots of bugs :)

--- src/DRiLDistributions/squashedDiagGaussian.jl
+++ src/DRiLDistributions/squashedDiagGaussian.jl
@@ -15,13 +15,13 @@ function Random.rand(rng::AbstractRNG, d::SquashedDiagGaussian, n::Integer)
     return [rand(rng, d) for _ in 1:n]
 end
 
-function logpdf(d::SquashedDiagGaussian, x::AbstractArray)
+function logpdf(d::SquashedDiagGaussian, x::AbstractArray{T}) where T<:Real
     gaussian_logpdf = logpdf(d.DiagGaussian, x)
     # More numerically stable formula: 2*(log(2) - x - softplus(-2*x)) instead of log(1 - tanh(x)^2)
     # https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/sac/core.py
     #TODO: type stability, getting Float64
     #TODO: make test for this
-    correction = 2 * (log(2) .- x .- Lux.softplus.(-2 .* x))
+    correction = 2 * (log(T(2)) .- x .- Lux.softplus.(-2 .* x))
     squashed_logpdf = gaussian_logpdf .- sum(correction)
     return squashed_logpdf
 end
