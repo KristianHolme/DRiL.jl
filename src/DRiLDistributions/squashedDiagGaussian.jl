1445dc2b7a405e14af5b17952558e6522a775454
Kristian Holme <holme.kristian@gmail.com>
2025-08-14 20:47:24 +0200

    try to fix runtime-dispatch in pullbacks

--- src/DRiLDistributions/squashedDiagGaussian.jl
+++ src/DRiLDistributions/squashedDiagGaussian.jl
@@ -26,14 +26,15 @@ function Random.rand(rng::AbstractRNG, d::SquashedDiagGaussian, n::Integer)
     return [rand(rng, d) for _ in 1:n]
 end
 
-function logpdf(d::SquashedDiagGaussian, x::AbstractArray{T}) where T<:Real
+function logpdf(d::SquashedDiagGaussian{T,M,S}, x::AbstractArray{T}) where {T<:Real,M,S}
     gaussian_action = atanh.(clamp.(x, -1 + d.epsilon, 1 - d.epsilon))
     gaussian_logpdf = logpdf(d.DiagGaussian, gaussian_action)
     # More numerically stable formula: 2*(log(2) - x - softplus(-2*x)) instead of log(1 - tanh(x)^2)
     # https://github.com/openai/spinningup/blob/master/spinup/algos/pytorch/sac/core.py
     #TODO: type stability, getting Float64
     #TODO: make test for this
-    correction = 2 * (log(T(2)) .- gaussian_action .- Lux.softplus.(-2 .* gaussian_action))
+    #TODO: fix runtime dispatch here
+    correction = T(2) * (log(T(2)) .- gaussian_action .- Lux.softplus.(-T(2) .* gaussian_action))
     squashed_logpdf = gaussian_logpdf .- sum(correction)
     return squashed_logpdf
 end
