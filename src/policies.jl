50befdf1b2c321b76541198d5eaf6e49d70b30cd
Kristian Holme <holme.kristian@gmail.com>
2025-10-03 23:51:55 +0200

    slight type improvement

--- src/policies.jl
+++ src/policies.jl
@@ -535,60 +535,49 @@ function (policy::DiscreteActorCriticPolicy)(obs::AbstractArray, ps, st)
 end
 
 # Type-stable feature extraction using dispatch
-function extract_features(policy::ContinuousActorCriticPolicy{<:Any, <:Any, <:Any, <:Any, SharedFeatures, <:Any, <:Any, <:Any, <:Any}, obs::AbstractArray, ps, st)
-    feats, feats_st = _apply_feature_extractor(policy.feature_extractor, obs, ps.feature_extractor, st.feature_extractor)
+function extract_features(policy::ContinuousActorCriticPolicy{O, A, N, C, SharedFeatures, FE, AH, CH, LS}, obs::AbstractArray, ps, st) where {O, A, N, C, FE, AH, CH, LS}
+    feats, feats_st = policy.feature_extractor(obs, ps.feature_extractor, st.feature_extractor)
     actor_feats = feats
     critic_feats = feats
     st = merge(st, (; feature_extractor = feats_st))
     return actor_feats, critic_feats, st
 end
 
-function extract_features(policy::ContinuousActorCriticPolicy{<:Any, <:Any, <:Any, <:Any, SeparateFeatures, <:Any, <:Any, <:Any, <:Any}, obs::AbstractArray, ps, st)
-    actor_feats, actor_feats_st = _apply_feature_extractor(policy.feature_extractor, obs, ps.actor_feature_extractor, st.actor_feature_extractor)
-    critic_feats, critic_feats_st = _apply_feature_extractor(policy.feature_extractor, obs, ps.critic_feature_extractor, st.critic_feature_extractor)
+function extract_features(policy::ContinuousActorCriticPolicy{O, A, N, C, SeparateFeatures, FE, AH, CH, LS}, obs::AbstractArray, ps, st) where {O, A, N, C, FE, AH, CH, LS}
+    actor_feats, actor_feats_st = policy.feature_extractor(obs, ps.actor_feature_extractor, st.actor_feature_extractor)
+    critic_feats, critic_feats_st = policy.feature_extractor(obs, ps.critic_feature_extractor, st.critic_feature_extractor)
     st = merge(st, (; actor_feature_extractor = actor_feats_st, critic_feature_extractor = critic_feats_st))
     return actor_feats, critic_feats, st
 end
 
 # For DiscreteActorCriticPolicy (3 type parameters)
-function extract_features(policy::DiscreteActorCriticPolicy{<:Any, <:Any, SharedFeatures, <:Any, <:Any, <:Any}, obs::AbstractArray, ps, st)
-    feats, feats_st = _apply_feature_extractor(policy.feature_extractor, obs, ps.feature_extractor, st.feature_extractor)
+function extract_features(policy::DiscreteActorCriticPolicy{O, A, SharedFeatures, FE, AH, CH}, obs::AbstractArray, ps, st) where {O, A, FE, AH, CH}
+    feats, feats_st = policy.feature_extractor(obs, ps.feature_extractor, st.feature_extractor)
     actor_feats = feats
     critic_feats = feats
     st = merge(st, (; feature_extractor = feats_st))
     return actor_feats, critic_feats, st
 end
 
-function extract_features(policy::DiscreteActorCriticPolicy{<:Any, <:Any, SeparateFeatures, <:Any, <:Any, <:Any}, obs::AbstractArray, ps, st)
-    actor_feats, actor_feats_st = _apply_feature_extractor(policy.feature_extractor, obs, ps.actor_feature_extractor, st.actor_feature_extractor)
-    critic_feats, critic_feats_st = _apply_feature_extractor(policy.feature_extractor, obs, ps.critic_feature_extractor, st.critic_feature_extractor)
+function extract_features(policy::DiscreteActorCriticPolicy{O, A, SeparateFeatures, FE, AH, CH}, obs::AbstractArray, ps, st) where {O, A, FE, AH, CH}
+    actor_feats, actor_feats_st = policy.feature_extractor(obs, ps.actor_feature_extractor, st.actor_feature_extractor)
+    critic_feats, critic_feats_st = policy.feature_extractor(obs, ps.critic_feature_extractor, st.critic_feature_extractor)
     st = merge(st, (; actor_feature_extractor = actor_feats_st, critic_feature_extractor = critic_feats_st))
     return actor_feats, critic_feats, st
 end
 
-# Function barriers to handle type-unstable layer calls
-@inline function _apply_feature_extractor(feature_extractor::FE, obs::AbstractArray, ps, st) where {FE <: AbstractLuxLayer}
-    return feature_extractor(obs, ps, st) #::Tuple{<:AbstractArray, <:NamedTuple}
-end
-
-@inline function _apply_actor_head(actor_head::AH, feats::AbstractArray, ps, st) where {AH <: AbstractLuxLayer}
-    return actor_head(feats, ps, st) #::Tuple{<:AbstractArray, <:NamedTuple}
-end
-
-@inline function _apply_critic_head(critic_head::CH, feats::AbstractArray, ps, st) where {CH <: AbstractLuxLayer}
-    return critic_head(feats, ps, st) #::Tuple{<:AbstractArray, <:NamedTuple}
-end
+# Direct calls to concrete layer fields keep inference intact
 
 function get_actions_from_features(policy::AbstractActorCriticPolicy, feats::AbstractArray, ps, st)
     # Use function barrier to isolate type instability
-    actions, actor_st = _apply_actor_head(policy.actor_head, copy(feats), ps.actor_head, st.actor_head)
+    actions, actor_st = policy.actor_head(copy(feats), ps.actor_head, st.actor_head)
     st = merge(st, (; actor_head = actor_st))
     return actions, st
 end
 
 function get_values_from_features(policy::AbstractActorCriticPolicy, feats::AbstractArray, ps, st)
     # Use function barrier to isolate type instability
-    values, critic_st = _apply_critic_head(policy.critic_head, feats, ps.critic_head, st.critic_head)
+    values, critic_st = policy.critic_head(feats, ps.critic_head, st.critic_head)
     st = merge(st, (; critic_head = critic_st))
     return values, st
 end
@@ -600,7 +589,7 @@ function get_values_from_features(policy::ContinuousActorCriticPolicy{<:Any, <:A
     inputs = vcat(feats, actions)
     # Use function barrier to isolate type instability
     #TODO: runtime dispatch
-    values, critic_st = _apply_critic_head(policy.critic_head, inputs, ps.critic_head, st.critic_head)
+    values, critic_st = policy.critic_head(inputs, ps.critic_head, st.critic_head)
     st = merge(st, (; critic_head = critic_st))
     return values, st
 end
