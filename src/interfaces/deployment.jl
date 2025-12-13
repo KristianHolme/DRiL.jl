71f7bfa3d512414a54bcf9e224370e6426d789b6
Kristian Holme <holme.kristian@gmail.com>
2025-12-13 22:27:34 +0100

    fix docs error for callable policy

--- src/interfaces/deployment.jl
+++ src/interfaces/deployment.jl
@@ -1,8 +1,10 @@
-abstract type AbstractPolicy end
-
 """
-    (::AbstractPolicy)(obs; deterministic::Bool = true, rng::AbstractRNG = Random.default_rng())
+    AbstractPolicy
+
+Abstract type for deployment policies. Subtypes are callable with signature:
+
+    (policy::AbstractPolicy)(obs; deterministic::Bool = true, rng::AbstractRNG = Random.default_rng())
 
 Return env-space actions for a single observation or a vector of observations.
 """
-function (::AbstractPolicy) end
+abstract type AbstractPolicy end
